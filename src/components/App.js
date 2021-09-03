5d6ada784510194d1e6375c689c0b276dd50a1c2
Carlos Eduardo Amancio De Oliveira <carlos.oliveira@fitbank.com.br>
2021-09-03 16:28:01 -0300

    first

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from 'react';
+import Scanner from './Scanner';
+import Result from './Result';
+import { string } from 'prop-types';
+
+const App = () => {
+    const [scanning, setScanning] = useState(false);
+    const [results, setResults] = useState([]);
+    const scannerRef = useRef(null);
+
+    return (
+        <div>
+            <button onClick={() => setScanning(!scanning)}>{scanning ? 'Stop' : 'Start'}</button>
+            <ul className="results">
+                {results.map((result) => (result.codeResult && <Result key={result.codeResult.code} result={result} />))}
+            </ul>
+            <div ref={scannerRef} style={{ position: 'relative', border: '3px solid red' }}>
+                {/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
+                <canvas className="drawingBuffer" style={{
+                    position: 'absolute',
+                    top: '0px',
+                    // left: '0px',
+                    // height: '100%',
+                    // width: '100%',
+
+                    border: '3px solid green',
+                }} width="640" height="480" />
+                {scanning ? <Scanner
+                    decoders={[
+                        'i2of5_reader',
+                    ]}
+                    scannerRef={scannerRef} onDetected={(result) => {
+                        if (result.length == 44) {
+                            console.log(result)
+                        }
+                    }} /> : null}
+            </div>
+        </div>
+    );
+};
+
+export default App;
